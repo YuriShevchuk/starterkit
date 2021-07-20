0d54f09781955ad8dc6bada8e515a59b9198ba0f
YuriShevchuk <yurii.shev4uk@gmail.com>
2021-07-20 20:14:36 +0300

    add vehicle-registry component

---
+++ src/app/vehicle-registry/vehicle-registry.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VehicleRegistryComponent } from './vehicle-registry.component';
+
+describe('VehicleRegistryComponent', () => {
+  let component: VehicleRegistryComponent;
+  let fixture: ComponentFixture<VehicleRegistryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VehicleRegistryComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VehicleRegistryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
